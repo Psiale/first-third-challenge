6605cd570bce6a0c39d677ee82bc8c5c2f237788
Alexis Sanchez <psialedev@gmail.com>
2021-04-29 13:01:04 -0500

    Finished the news item component

--- src/scripts/news.js
+++ src/scripts/news.js
@@ -1,4 +1,19 @@
 import htmlGenerator from './domTools';
 const news = (title, description) => {
-    
+    const mainContainer = htmlGenerator('div', 'news-main-container');
+    const imageContainer = htmlGenerator('div', 'news-image-container');
+    const newsContainer = htmlGenerator('div', 'news-description-container');
+
+    // imageContainer child
+    const imagePlaceHolder = htmlGenerator('div', 'image-placeholder');
+
+    // newsContainer childs
+    const newsTitle = htmlGenerator('H3', 'news-title', title);
+    const newsDescription = htmlGenerator('p', 'news-description', description);
+
+    // appending
+    imageContainer.appendChild(imagePlaceHolder);
+    newsContainer.append(newsTitle, newsDescription);
+    mainContainer.append(imageContainer, newsContainer);
 }
+export default news;
